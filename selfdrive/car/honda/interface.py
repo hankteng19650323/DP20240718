6a4c121cddbe67ce24f249b2ba8db41b0af32d3e
Quocuong83 <67253220+Quocuong83@users.noreply.github.com>
2020-09-26 00:26:43 -0500

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -330,7 +330,12 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.0  # as spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 1000], [0, 1000]]  # TODO: determine if there is a dead zone at the top end
       tire_stiffness_factor = 0.444
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
+      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
+      ret.lateralTuning.init('indi')
+      ret.lateralTuning.indi.innerLoopGain = 10.0
+      ret.lateralTuning.indi.outerLoopGain = 14.0
+      ret.lateralTuning.indi.timeConstant = 4.5
+      ret.lateralTuning.indi.actuatorEffectiveness = 3.5
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
@@ -434,8 +439,8 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startAccel = 0.5
 
-    ret.steerActuatorDelay = 0.1
-    ret.steerRateCost = 0.5
+    ret.steerActuatorDelay = 0.6
+    ret.steerRateCost = 0.2
     ret.steerLimitTimer = 0.8
 
     return ret
