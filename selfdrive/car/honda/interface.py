efef930097606fe5dc49b5dc223574ba370b8dbc
loveloveses <52433822+loveloveses@users.noreply.github.com>
2021-08-23 04:29:39 -0500

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -490,13 +490,9 @@ class CarInterface(CarInterfaceBase):
     ret.yawRate = self.VM.yaw_rate(ret.steeringAngle * CV.DEG_TO_RAD, ret.vEgo)
     # FIXME: read sendcan for brakelights
     brakelights_threshold = 0.02 if self.CS.CP.carFingerprint == CAR.CIVIC else 0.1
-    brakelights_threshold_BOSCH = 0.47
-    if self.CS.CP.carFingerprint == CAR.CRV_HYBRID:
-      ret.brakeLights = bool(self.CS.brake_switch or
+    brakelights_threshold_BOSCH = 0.45
+    ret.brakeLights = bool(self.CS.brake_switch or
                            self.CS.brake_lights or self.CS.user_brake > brakelights_threshold_BOSCH)
-    else:
-      ret.brakeLights = bool(self.CS.brake_switch or
-                           c.actuators.brake > brakelights_threshold)
 
     # dp
     ret.lkMode = self.CS.lkMode
