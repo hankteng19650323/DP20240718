2d17892f5ed62a2cca90209568d6475a9d7c2c2d
CT921 <tim.chen242@gmail.com>
2021-09-24 12:11:30 +0800

    Add TOYOTA PRIUS ALPHA support

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -33,7 +33,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = False # Toyota starts braking more when it thinks you want to stop
 
-    if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H]:  # These cars use LQR/INDI
+    if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H, CAR.PRIUS_ALPHA]:  # These cars use LQR/INDI
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
 
@@ -195,6 +195,26 @@ class CarInterface(CarInterfaceBase):
           ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.05]]
           ret.lateralTuning.pid.kf = 0.00004
           break
+
+    elif candidate in [CAR.PRIUS_ALPHA]:
+      stop_and_go = True
+      ret.safetyParam = 73
+      ret.wheelbase = 2.78
+      ret.steerRatio = 15.5
+      tire_stiffness_factor = 0.5533
+      ret.mass = 4387. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.05]]
+      ret.lateralTuning.pid.kf = 0.00006
+	  
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 1500.0
+      ret.lateralTuning.lqr.ki = 0.05
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
 
     elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2]:
       stop_and_go = True
