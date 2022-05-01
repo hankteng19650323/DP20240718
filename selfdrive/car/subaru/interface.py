a4ef04587899b4fa8f303e9e1cbd4db0371d2403
Hikari1023 <93507091+Hikari1023@users.noreply.github.com>
2022-05-01 15:57:16 +0800

    Enable subaru support for some cars
    CAR.FORESTER_PREGLOBAL, CAR.LEGACY_PREGLOBAL, CAR.OUTBACK_PREGLOBAL, CAR.OUTBACK_PREGLOBAL_2018

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -22,7 +22,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.subaru)]
       ret.enableBsm = 0x228 in fingerprint[0]
 
-    ret.dashcamOnly = candidate in PREGLOBAL_CARS
+    #ret.dashcamOnly = candidate in PREGLOBAL_CARS
 
     ret.steerRateCost = 0.7
     ret.steerLimitTimer = 0.4
