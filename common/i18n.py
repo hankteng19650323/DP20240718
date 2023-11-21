4ab25ad902327de6aabbeff1cc4ef287bc59cd45
NASSER ALKHALDI <36295124+NMK-Ai@users.noreply.github.com>
2023-11-21 18:57:58 +0300

    NMK

--- common/i18n.py
+++ common/i18n.py
@@ -2,9 +2,10 @@ import gettext
 from common.params import Params
 
 locale_dir = "/data/openpilot/selfdrive/assets/locales"
-# supported_language = ["en-US", "zh-TW", "zh-CN", "ja-JP", "ko-KR"]
+# supported_language = ["en-US","ar-SA", "zh-TW", "zh-CN", "ja-JP", "ko-KR"]
 supported_languages = {
   "main_en": "en-US",
+  "main_ar_SA": "ar-SA",
   "main_zh-CHT": "zh-TW",
   "main_zh-CHS": "zh-CN",
   "main_ko": "ko-KR",
